38a23d3ba4081cecbffc3b372f3a3fa8b1e4000a
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-07-09 17:19:55 +0100

    Working pretty much

--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
+const int WPM = 140; //desired speed
 
-#define DELAY 200
+#define AVG_CHAR_PER_WORD 6 /* https://diuna.biz/length-of-words-average-number-of-characters-in-a-word/ */
 
+const int DELAY = 10e5 / ((WPM * AVG_CHAR_PER_WORD) / 60.0f);
 
 void usage(void)
-{}
+{
+  printf("\n");
+}
+
+void print_delay(FILE *file)
+{
+  char c;
+  while((c = fgetc(file)) != EOF){
+    putchar(c);
+    usleep(DELAY);
+    fflush(stdout);
+  }
+}
 
 int main(int argc, char *argv[])
 {
   if(argc != 2){
-    exit(1);
+    usage();
+    exit(-1);
   }
-  printf("%s",argv[1]);
-  return 0;
-  FILE* f = fopen(argv[1], "r");
-  sleep(200);
+
+  FILE* file = fopen(argv[1], "r");
+  if(!file){
+    printf("Error: %s\n", strerror(errno));
+    exit(-1);
+  }
+
+  print_delay(file);
+
+  fclose(file);
+
   return 0;
 }
