e28829763a18077c2f5f4f3ba555f95e21d3cce0
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-07-10 11:37:18 +0100

    added signal handler to reset terminal on interrupt

--- main.c
+++ main.c
@@ -3,18 +3,33 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <termios.h>
+#include <signal.h>
 
 /*
  * Program to output files character by character to simulate typing.
  * F Fitzgerald 2019
  */
 
-#define AVG_CHAR_PER_WORD 6
+#define AVG_CHAR_PER_WORD 6 //guesstimate
+
+void cleanup()
+{
+  printf("\033c");
+  exit(0);
+}
+
+// print errno and exit program
+void die()
+{
+  fprintf(stderr, "Error: %s\n", strerror(errno));
+  exit(-1);
+}
 
 void usage(char *argv[])
 {
-  printf("Outputs a file character by character using a simulated typing speed.\n");
-  printf("Usage: %s [-w words_per_min] file\n", argv[0]);
+  fprintf(stderr, "Outputs a file character by character using a simulated typing speed.\n");
+  fprintf(stderr, "Usage: %s [-w words_per_min] file\n", argv[0]);
 }
 
 void print_delay(FILE *file, int delay)
@@ -23,13 +38,17 @@ void print_delay(FILE *file, int delay)
   while((c = fgetc(file)) != EOF){
     putchar(c);
     usleep(delay);
-    fflush(stdout);
+    fflush(stdout); //need to flush after each char to see the output
   }
 }
 
 int main(int argc, char *argv[])
 {
+  signal(SIGINT, cleanup); //reset terminal when we Ctrl-C to exit
+
   int wpm = 140; //default words per minute
+
+  /* handle args */
   int opt;
   while((opt = getopt(argc, argv, "w:")) != -1){
     switch(opt){
@@ -47,21 +66,31 @@ int main(int argc, char *argv[])
     exit(-1);
   }
 
+  /* Open the input file */
   FILE* file = fopen(argv[optind], "r");
-  if(!file){
-    printf("Error: %s\n", strerror(errno));
-    exit(-1);
-  }
+  if(!file) die();
 
-  //first clear the console for full fake effect
-  printf("\033c");
+  /* set terminal to not echo any key inputs */
+  struct termios term;
 
-  //typing delay is based on characters per second
-  //since we are using usleep seconds are microseconds
+  if(tcgetattr(STDIN_FILENO, &term)) die();
+
+  term.c_lflag &= ~((tcflag_t) ECHO); //turn off echo
+
+  if(tcsetattr(STDIN_FILENO, TCSANOW, &term)) die();
+
+  /* Clear the console for a better effect */
+  printf("\033c"); //clear console
+  printf("\x1b[?;25;l"); //hide cursor
+
+  /* Typing delay is based on characters per second
+   * since we are using usleep seconds are microseconds */
   int delay = 10e5 / ((wpm * AVG_CHAR_PER_WORD) / 60.0f);
   print_delay(file, delay);
 
   fclose(file);
+
+  cleanup();
 
   return 0;
 }
